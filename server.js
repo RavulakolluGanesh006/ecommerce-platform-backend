8d0cb39804014e19f00f1cbcfa99cc0f307adbfc
nikhil <ravulakolluganesh06@gmail.com>
2025-06-24 17:40:43 +0530

    backend logic

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running...");
+    });
+  })
+  .catch(err => console.error(err));
