63128716984009ca80511c1761cbc5d0f713a792
nikhil <ravulakolluganesh06@gmail.com>
2025-07-11 17:17:01 +0530

    all set

--- controllers/productController.js
+++ controllers/productController.js
@@ -35,3 +35,22 @@ res.json({ message: "Product Updated" });
     }
 
 }
+
+
+exports.getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(404).json({ message: "Product not found" });
+  res.json(product);
+};
+
+
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find(); // Get all products from MongoDB
+    res.json(products); // Return them as JSON
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct,deleteProduct,updateProduct,getProducts } = require("../controllers/productController");
+const { createProduct,deleteProduct,updateProduct, getProductById, getProducts } = require("../controllers/productController");
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
 router.post("/add-product", authMiddleware, adminMiddleware, createProduct);
-//router.get("/products",getProducts)
+router.get("/products/:id", getProductById);
+router.get("/products", getProducts);
+
 router.delete("/products/:id",authMiddleware, adminMiddleware, deleteProduct)
 router.put("/products/:id",authMiddleware, adminMiddleware, updateProduct)
 module.exports = router;
